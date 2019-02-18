e8c8e2271e626b54f75520a197aff773e543f801
Rodwyn <rodwynmoreno@gmail.com>
2019-02-18 17:34:35 -0600

    Added product router. Added product viw.

--- product.js
+++ product.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-let product = [];
+let product = []
 
 router.post('/', (req, res, next) => {
-  product = [...req.body.product || []];
-  if (req.body.remove) product.splice(req.body.remove, 1);
-  if (req.body.new) product.push({});
+  product = [...req.body.product || []]
+  if (req.body.remove) product.splice(req.body.remove, 1)
+  if (req.body.new) product.push({})
 
-  next();
-});
+  next()
+})
 
 router.use('/', (req, res) => {
-  res.render('product', {
-    title: 'Product list',
-    product
-  })
-});
+  res.render('product', { title: 'Product list', product })
+})
 
-module.export = router;
+module.exports = router
