89e8196dfb46d45af47e109f53bc1ce1d1e0c6a7
Rodwyn <rodwynmoreno@gmail.com>
2019-02-28 14:50:50 -0600

    Revert "Revert "Merge pull request #2 from rodwyn/revert-1-feature/F01-Add-user-authentication""
    This reverts commit 262d462746cf484f80b3cee96f461b399113ad2c.

--- product.js
+++ product.js
@@ -1,26 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-const productsByUser = {};
+let product = []
 
 router.post('/', (req, res, next) => {
-  const product = [...req.body.product || []]
+  product = [...req.body.product || []]
   if (req.body.remove) product.splice(req.body.remove, 1)
   if (req.body.new) product.push({})
-
-  productsByUser[req.userContext.userinfo.sub] = product
 
   next()
 })
 
 router.use('/', (req, res) => {
-  const product =  productsByUser[req.userContext.userinfo.sub] || []
-
-  res.render('product', {
-    title: 'Product list',
-    product,
-    userinfo: req.userContext.userinfo
-  })
+  res.render('product', { title: 'Product list', product })
 })
 
 module.exports = router
