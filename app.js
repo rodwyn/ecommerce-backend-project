2b15a29556da2c356bf08d6ba76742aac4353d82
Rodwyn <rodwynmoreno@gmail.com>
2019-03-13 18:15:27 -0600

    F04 Added middlewares.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import path from 'path';
 import DB from './config/db';
+import CORS from './middleware/cors';
+import notFound from './middleware/notFound';
+import error from './middleware/error';
 
 const app = express();
 const PORT = 3000;
 
 DB.connect();
+
+app.use(express.json());
+app.use(CORS.handleCors);
+app.use(notFound);
+app.use(error);
 
 app.get('/', (req, res) => {
 	res.send('hello world');
